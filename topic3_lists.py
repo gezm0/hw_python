57ea6a71a009636fff6ab6c8b2754acf45dd6874
gezm0 <eremin@ryazan.net.ru>
2021-11-22 16:24:12 +0300

    i'm done for today

--- topic3_lists.py
+++ topic3_lists.py
@@ -48,7 +48,7 @@ print (len(fruits1))
 print('We have in our grocery:')
 fruit_num = 1
 for fruit in fruits:
-    print(fruit_num, fruit)
+    print(fruit_num,')', fruit)
     fruit_num += 1
 print("Total:", (fruit_num)-1)
 
@@ -61,3 +61,35 @@ print('There are', len(fruits_all), 'fruits in grocery')
 
 print("First item by alphabet is:", min(fruits_all))
 print("Last item by alphabet is:", max(fruits_all))
+
+# any(list) logical 'or', all(list) logical 'and'
+
+empty_lyst = []
+not_really_empty_list = [1, 2, 3, 4, [], 6, 7]
+
+print('Is there any data in list?', any(empty_lyst))
+print('Is there any data in list?', any(not_really_empty_list))
+
+print('Is there all items in list are filled by equal data?', all(empty_lyst))
+print('Is there all items in list are filled by equal data?', all(not_really_empty_list))
+
+# list(seq) an item by letters
+
+print(list('awkwardly'))
+awkw = list('awkwardly')
+print(awkw[3:6],awkw[7:])
+
+print(list(fruits_all[5]))
+
+# List Methods
+# append at the last of list
+fruits.append('mandarin')
+print(fruits)
+
+print(fruits_all.count('orange'))
+print(fruits_all.index('plum'))
+
+fruits_all.remove('orange')
+print(fruits_all)
+fruits_all.pop(2)
+print(fruits_all)
